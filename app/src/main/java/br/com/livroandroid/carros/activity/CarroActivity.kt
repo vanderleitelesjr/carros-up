243f981bb9eb8296a70d3723122ea9ea0826bf6c
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-09-15 13:34:05 -0300

    ok

---
+++ app/src/main/java/br/com/livroandroid/carros/activity/CarroActivity.kt
@@ -0,0 +1,14 @@
+package br.com.livroandroid.carros.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.livroandroid.carros.R
+
+class CarroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_carro)
+    }
+}
