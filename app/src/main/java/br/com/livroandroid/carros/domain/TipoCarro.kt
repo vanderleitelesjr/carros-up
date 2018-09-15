7e1d6a461cd089532e16a96f4b4ceb71819af96e
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-09-15 10:59:46 -0300

    ok

--- app/src/main/java/br/com/livroandroid/carros/domain/TipoCarro.kt
+++ app/src/main/java/br/com/livroandroid/carros/domain/TipoCarro.kt
@@ -6,5 +6,6 @@ import br.com.livroandroid.carros.R
 enum class TipoCarro(@StringRes val abacaxi: Int) {
     Classicos(R.string.classico),
     Esportivos(R.string.esportivos),
-    Luxo(R.string.luxo)
+    Luxo(R.string.luxo),
+    Favorito(R.string.favoritos)
 }
